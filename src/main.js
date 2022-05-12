1932d38e4fa98e654d4790163e69017c18128b0c
jlarata <ariel@cfee.gob.ar>
2022-05-12 00:05:05 -0300

    hola dani (L)

---
+++ src/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+function sujeto() {
+
+document.getElementById("lacuestion").classList.add("esesta");
+
+}
+
+function predicado() {
+
+document.getElementById("lacuestion").classList.add("ytambienesesta");
+
+}
